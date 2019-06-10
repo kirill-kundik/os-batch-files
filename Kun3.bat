c1344242cfc75b7822441d6818ee2e6be1183e50
kyrylo_kundik <goliathusua@gmail.com>
2019-06-10 16:00:42 +0300

    Lab3 Lab4 done

--- Kun3.bat
+++ Kun3.bat
@@ -1,29 +1,56 @@
 @ECHO OFF
 
 SETLOCAL enableextensions disabledelayedexpansion
-
+:: Checking dates file existings
+IF NOT EXIST Dates.txt GOTO ERROR
+:: getting today and tomorrow dates
 FOR /f "tokens=2 delims==" %%a IN ('wmic OS Get localdatetime /value') DO SET "dt=%%a"
+
 SET "td.YY=%dt:~2,2%"
 SET "td.YYYY=%dt:~0,4%"
+
 SET "td.MM=%dt:~4,2%"
 SET "td.DD=%dt:~6,2%"
 
-rem Remove padding from date elements and increase day
+:: remove padding from date elements and increase day
 SET /a "y=%td.YYYY%", "m=100%td.MM% %% 100", "d=(100%td.DD% %% 100)+1"
-rem Calculate month length
-SET /a "ml=30+((m+m/8) %% 2)" & if %m% equ 2 SET /a "ml=ml-2+(3-y %% 4)/3-(99-y %% 100)/99+(399-y %% 400)/399"
-rem Adjust day / month / year for tomorrow date
-If %d% gtr %ml% SET /a "d=1", "m=(m %% 12)+1", "y+=(%m%/12)"
 
-rem Pad date elements and SET tomorrow variables
+:: calculate month length
+SET /a "ml=30+((m+m/8) %% 2)" & if %m% equ 2 SET /a "ml=ml-2+(3-y %% 4)/3-(99-y %% 100)/99+(399-y %% 400)/399"
+
+:: adjust day / month / year for tomorrow date
+IF %d% GTR %ml% SET /a "d=1", "m=(m %% 12)+1", "y+=(%m%/12)"
+
+:: pad date elements and SET tomorrow variables
 SET /a "m+=100", "d+=100"
 
 SET "tm.YYYY=%y%"
 SET "tm.YY=%y:~-2%"
+
 SET "tm.MM=%m:~-2%"
 SET "tm.DD=%d:~-2%"
 
-ECHO Today   : %td.YYYY% / %td.MM% / %td.DD%
-ECHO Tomorrow: %tm.YYYY% / %tm.MM% / %tm.DD%
+SET today=%td.DD%.%td.MM%.%td.YYYY%
+SET tomorrow=%tm.DD%.%tm.MM%.%tm.YYYY%
+
+:: echoing today and tomorrow dates
+ECHO Today   : %today%
+ECHO Tomorrow: %tomorrow%
+
+ECHO Your notifications for upcoming events:
+:: loop through dates file
+FOR /f "tokens=1,2 delims=:" %%A IN (Dates.txt) DO (
+  IF "%%B"==" %tomorrow%" (
+    echo You are going %%A tomorrow!
+    ) ELSE (
+      :: ECHO You don't have other events for tomorrow!
+      )
+)
+ECHO You don't have other events for tomorrow!
+GOTO EOF
+
+:ERROR
+
+ECHO DATES.TXT WAS NOT FOUND IN CURRENT DIRECTORY!
 
 :EOF
--- Kun4.bat
+++ Kun4.bat
@@ -1,25 +1,32 @@
-@ECHO OFF
-SETLOCAL disableDelayedExpansion
-SET /P root=Please Enter a Number from 0:
+@ECHO OFF 
 
-SET "prevTest=none"
-SET "prevFile=none"
-FOR /f "tokens=1-3 delims=:" %%A IN (
-  '"(FOR /r "%root%" %%F IN (*) do @ECHO %%~znxF:%%~fF:)|sort"'
-) DO (
-  SET "currTest=%%A"
-  SET "currFile=%%B:%%C"
-  SETLOCAL enableDelayedExpansion
-  SET "match="
-  If !currTest! equ !prevTest! fc /b "!prevFile!" "!currFile!" >nul && SET match=1
-  If defined match (
-    ECHO del "!currFile!"
-    ENDLOCAL
-  ) else (
-    ENDLOCAL
-    SET "prevTest=%%A"
-    SET "prevFile=%%B:%%C"
-  )
+ECHO Set path to test folder, which will be choosen for file equality checking
+
+:searchFolder
+SET /p root=Enter the path:
+IF EXIST %root% (
+  GOTO :inspectMatch
+  ) ELSE (
+    ECHO There arent any directories with such name - ERROR
+    GOTO :searchFolder
 )
 
+:inspectMatch
+FOR %%n IN (%root%\*) DO (
+  ECHO Same files to the %%n: >> List.txt
+  :: ECHO %%n
+  FOR /r %root%\ %%i IN (*) DO (
+    :: ECHO %%i
+    :: ECHO %%n
+    IF NOT %%n==%%i (
+      fc /b %%n %%i >NUL && ECHO File: %%~nxi  Path: %%~dpi  >> List.txt
+    )
+  )
+ :: ECHO No more equal files >> List.txt
+ ECHO. >> List.txt
+)
+
+GOTO :EOF
+
 :EOF
+EXIT
